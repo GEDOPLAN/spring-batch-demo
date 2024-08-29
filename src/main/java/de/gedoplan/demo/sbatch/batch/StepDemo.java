413f603c56376a41dfffd5dc552b450f236aca10
Dominik Mathmann <dominik.mathmann@gedoplan.de>
2024-08-29 14:13:31 +0200

    improve step demo

--- src/main/java/de/gedoplan/demo/sbatch/batch/StepDemo.java
+++ src/main/java/de/gedoplan/demo/sbatch/batch/StepDemo.java
@@ -12,6 +12,7 @@ import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.Chunk;
+import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.database.AbstractPagingItemReader;
 import org.springframework.batch.repeat.RepeatStatus;
@@ -28,6 +29,7 @@ import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Configuration
 @RequiredArgsConstructor
@@ -51,13 +53,13 @@ public class StepDemo {
         return new StepBuilder("cleanDB", jobRepository)
                 .tasklet((contribution, chunkContext) -> {
                     JobExecution last = jobRepository.getLastJobExecution(jobExecution.getJobInstance().getJobName(), jobExecution.getJobParameters());
-                    if(last!=null && !last.getExitStatus().equals(ExitStatus.COMPLETED)){
+                    if (last != null && !last.getExitStatus().equals(ExitStatus.COMPLETED)) {
                         log.info("Last run was NOT successfully, we keep our data");
                     } else {
                         log.info("Last run was successfully, lets clean the database for a fresh copy");
                         productRepository.deleteAll();
                     }
-                        return RepeatStatus.FINISHED;
+                    return RepeatStatus.FINISHED;
                 }, ptm)
                 .allowStartIfComplete(true)
                 .build();
@@ -67,24 +69,25 @@ public class StepDemo {
         return new StepBuilder("importProducts", jobRepository)
                 .<Product, Product>chunk(10, ptm)
                 .reader(dummyJsonItemReader)
+//                .reader(readProduct(null))
                 .writer(writeProduct())
                 .faultTolerant()
                 .retry(IOException.class)
                 .retry(RestClientException.class)
                 .retryLimit(5)
                 .allowStartIfComplete(true)
-                .listener(idToContext( null))
+                .listener(idToContext(null))
                 .build();
     }
 
 
     @Bean
     @StepScope
-    public ItemWriteListener<Product> idToContext(@Value("#{stepExecution}") StepExecution stepExecution){
+    public ItemWriteListener<Product> idToContext(@Value("#{stepExecution}") StepExecution stepExecution) {
         return new ItemWriteListener<>() {
             @Override
             public void afterWrite(Chunk<? extends Product> items) {
-                stepExecution.getJobExecution().getExecutionContext().put("lastId", items.getItems().get(items.size()-1).getId());
+                stepExecution.getJobExecution().getExecutionContext().put("lastId", items.getItems().get(items.size() - 1).getId());
             }
         };
     }
@@ -92,17 +95,29 @@ public class StepDemo {
 
     private ItemWriter<Product> writeProduct() {
         return (products) -> {
-            for (Product p : products) {
+                productRepository.saveAll(products);
 // let's fail for some reason
+//            for (Product p : products) {
 //                if (p.getId().equals(new BigDecimal(50))){
-                if (p.getId().compareTo(new BigDecimal(125)) > 0) {
-                    throw new IOException("IDs bigger then 125 are unfriendly");
-                }
-                productRepository.save(p);
-            }
+//                if (p.getId().compareTo(new BigDecimal(125)) > 0) {
+//                    throw new IOException("IDs bigger then 125 are unfriendly");
+//                }
+//            }
             productRepository.flush();
         };
+    }
 
+    @Bean
+    @StepScope
+    public ItemReader<Product> readProduct(@Value("#{stepExecution}") StepExecution stepExecution) {
+        return () -> {
+            BigDecimal newId = Optional.ofNullable(stepExecution.getExecutionContext().get("lastId")).map(v -> (BigDecimal) v).orElse(BigDecimal.ZERO).add(BigDecimal.ONE);
+            if(newId.compareTo(new BigDecimal(250))>0) {
+                return null;
+            }
+            stepExecution.getExecutionContext().put("lastId", newId);
+            return Product.builder().title("Dummy").id(newId).build();
+        };
     }
 
     @Data
@@ -112,7 +127,7 @@ public class StepDemo {
 
     @Component
     @StepScope
-    public static class DummyJsonItemReader extends  AbstractPagingItemReader<Product>{
+    public static class DummyJsonItemReader extends AbstractPagingItemReader<Product> {
 
         private final RestTemplate dummyJson;
 
